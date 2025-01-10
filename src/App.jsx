6f05748d2c68e85c0a057f5d55f8cffc531dc834
unknown <rajkarnikarupekkha02@gmail.com>
2025-01-10 17:28:49 +0545

    increment decrement cound

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
-import Button from "./components/button"
+import Button from "./components/button";
+import bulbOff from "./assets/images/bulbOff-removebg-preview.png";
+
+
 //usestate Hooks
 import { useState } from "react";
 // passing props
@@ -16,12 +19,20 @@ const user__data = {
 function App() {
 
   const [count, setCount] = useState(0);
+  // const [bulb, setBulb] = useState(0);
     
-  const handleClick1 = () => {
+  const increaseCount = () => {
       setCount(count + 1);
   };
-  const handleClick2 = () => {
-      setCount(count + 2);
+
+  const decreaseCount = () => {
+    if(count > 0){
+      setCount(count - 1);
+    }
+  };
+
+  const switchToggle = () =>{
+
   };
 
   return (
@@ -30,9 +41,9 @@ function App() {
        <div className="w-full h-full flex items-center justify-center flex-col gap-6">
         <h1 className='font-bold text-blue-400 text-center'>Increase or Decrease the number</h1>
         <div className="flex gap-3">
-          <Button handleClick={handleClick1}  text="Click me" />
+          <Button handleClick={increaseCount}  text="Increase Count" />
           {count}
-          <Button handleClick={handleClick2}  text="Click Here" />
+          <Button handleClick={decreaseCount}  text="Decrease Count" />
         </div>
         <div className="flex flex-col border border-blue-400 p-[40px] rounded-[4px] ">
           <h2 className="mb-[10px] font-bold  text-blue-400">User data</h2>
@@ -42,6 +53,13 @@ function App() {
           <span>
             Lastname: <span className="font-bold">{user__data.lname}</span>
           </span>
+        </div>
+
+        <div className="flex flex-col border border-blue-400 p-[40px] rounded-[4px] ">
+          <div className="flex justify-center flex-col gap-3 items-center p-[30px]">
+            <img className="w-[100px] h-[auto]" src={bulbOff} alt="" />
+            <Button handleClick={switchToggle} text="Switch on the light" />
+          </div>
         </div>
 
        </div>
